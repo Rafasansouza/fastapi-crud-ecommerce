662f4a12a09ce47753da8e015d891b3465626ddc
Rafael Souza <rafasansou1@gmail.com>
2025-05-13 15:26:22 -0300

    aprimorando o projeto com variaveis de ambiente

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
-POSTGRES_DATABASE_URL = "postgresql://user:password@postgres/mydatabase"
+# Carregando o .env em outra pasta
+dotenv_path = Path(__file__).resolve().parents[1] / ".env"
+load_dotenv(dotenv_path)
+
+POSTGRES_DATABASE_URL = os.getenv("DATABASE_URL")  #"postgresql://user:password@postgres/mydatabase"
 
 engine = create_engine(POSTGRES_DATABASE_URL)
 
